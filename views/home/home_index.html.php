38fea2a3add6f39908cb9744ac4e8291456d2922
ikkrit <johan.car@protonmail.com>
2022-12-15 23:19:22 +0100

    home

---
+++ views/home/home_index.html.php
@@ -0,0 +1,7 @@
+<section class="home">
+    <h1 class="home__title">WebMangas</h1>
+
+    <div class="home__container">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ut laudantium saepe quis in accusamus ex quam molestiae culpa quibusdam obcaecati architecto eligendi pariatur voluptas, quae magnam modi ipsam. Consequuntur.</p>
+    </div>
+</section>
