515592af137fca505cf4ebe2160ec41c065dcce4
ikkrit <johan.car@protonmail.com>
2022-12-17 18:07:12 +0100

    structure

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,84 @@
 // HEADER HOME
 
-const imgSlider = (anything) => {
-  document.querySelector('.img__item').src = anything;
-};
+let menu = document.querySelector("#menu-bars");
+let navbar = document.querySelector(".navbar");
 
-const changeCirclecolor = (color) => {
-  const circle = document.querySelector('.circle');
-  circle.style.background = color;
-};
+menu.onclick = () => {
+  menu.classList.toggle('fa-time');
+  navbar.classList.toggle('active');
+}
 
-const toggleMenu = () => {
-  let menuToggle = document.querySelector('.toggle');
-  let navigation = document.querySelector('.navigation');
+var swiper = new Swiper(".home-slider",{
+  spaceBetween:30,
+  centeredSlides:true,
+  autoplay:{
+    delay:7500,
+    disableOnInteration:false,
+  },
+  pagination:{
+    el:".swiper-pagination",
+    clickable:true,
+  },
+  loop:true,
+});
 
-  menuToggle.classList.toggle('active');
-  navigation.classList.toggle('active');
-};
+var swiper = new Swiper(".anime-slider",{
+  slidesPerView:4,
+  spaceBetween:30,
+  centeredSlides:true,
+  autoplay:{
+    delay:4500,
+    disableOnInteration:false,
+  },
+  pagination:{
+    el:".swiper-pagination",
+    clickable:true,
+  },
+  loop:true,
+});
+
+var swiper = new Swiper(".action-slider",{
+  slidesPerView:4,
+  spaceBetween:30,
+  centeredSlides:true,
+  autoplay:{
+    delay:3500,
+    disableOnInteration:false,
+  },
+  pagination:{
+    el:".swiper-pagination",
+    clickable:true,
+  },
+  loop:true,
+});
+
+var swiper = new Swiper(".child-slider",{
+  slidesPerView:4,
+  spaceBetween:30,
+  centeredSlides:true,
+  autoplay:{
+    delay:2500,
+    disableOnInteration:false,
+  },
+  pagination:{
+    el:".swiper-pagination",
+    clickable:true,
+  },
+  loop:true,
+});
+
+var swiper = new Swiper(".family-slider",{
+  slidesPerView:4,
+  spaceBetween:30,
+  centeredSlides:true,
+  autoplay:{
+    delay:1500,
+    disableOnInteration:false,
+  },
+  pagination:{
+    el:".swiper-pagination",
+    clickable:true,
+  },
+  loop:true,
+});
+
--- views/home/home_index.html.php
+++ views/home/home_index.html.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css">
     <link rel="stylesheet" href="/assets/css/home.css">
     <title>Anime</title>
 </head>
@@ -22,12 +23,94 @@
         </nav>
 
         <div class="icons">
-            <i class="fas fa-bars" id="bars"></i>
+            <i class="fas fa-bars" id="menu-bars"></i>
             <i class="fas fa-search" id="search-icon"></i>
-            <i class="fas fa-heart" id="heart"></i>
+            <a href="#" class="fas fa-heart"></a>
         </div>
 
     </header>
+
+    <section class="home" id="home">
+
+        <div class="swiper home-slider">
+            <div class="swiper-wrapper">
+
+                <div class="swiper-slide">
+                    <div class="box" style="background: url(/assets/img/home/manhua_1.png) no-repeat;">
+                        <div class="content">
+                            <h3>Kakegurui</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt explicabo fugiat atque? Doloribus numquam, voluptas aperiam ea officiis quam cupiditate quibusdam similique incidunt, tempore accusamus ullam ut, magnam ad? Quis. </p>
+                            <a href="#" class="btn">Download</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="swiper-slide">
+                    <div class="box second" style="background: url(/assets/img/home/manhua_1.png) no-repeat;">
+                        <div class="content">
+                            <h3>Kakegurui</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt explicabo fugiat atque? Doloribus numquam, voluptas aperiam ea officiis quam cupiditate quibusdam similique incidunt, tempore accusamus ullam ut, magnam ad? Quis. </p>
+                            <a href="#" class="btn">Download</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="swiper-slide">
+                    <div class="box" style="background: url(/assets/img/home/manhua_1.png) no-repeat;">
+                        <div class="content">
+                            <h3>Kakegurui</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt explicabo fugiat atque? Doloribus numquam, voluptas aperiam ea officiis quam cupiditate quibusdam similique incidunt, tempore accusamus ullam ut, magnam ad? Quis. </p>
+                            <a href="#" class="btn">Download</a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </section>
+
+    <section class="anime" id="anime">
+        <h1 class="heading">Popular on Anime</h1>
+
+        <div class="swiper anime-slider">
+
+            <div class="swiper-wrapper">
+
+                <div class="swiper-slide">
+                    <div class="box" style="background: url(/assets/img/home/manhua_1.png) no-repeat"></div>
+                    <div class="content">
+                        <h3>Naruto</h3>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum dicta accusamus quis, et alias sit? Quisquam beatae, vero inventore cupiditate ullam, quasi repellat esse hic maxime veritatis odio iusto suscipit!</p>
+                        <a href="#" class="btn">Download</a>
+                    </div>
+                </div>
+
+                <div class="swiper-slide">
+                    <div class="box" style="background: url(/assets/img/home/manhua_1.png) no-repeat"></div>
+                    <div class="content">
+                        <h3>Naruto</h3>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum dicta accusamus quis, et alias sit? Quisquam beatae, vero inventore cupiditate ullam, quasi repellat esse hic maxime veritatis odio iusto suscipit!</p>
+                        <a href="#" class="btn">Download</a>
+                    </div>
+                </div>
+
+                <div class="swiper-slide">
+                    <div class="box" style="background: url(/assets/img/home/manhua_1.png) no-repeat"></div>
+                    <div class="content">
+                        <h3>Naruto</h3>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum dicta accusamus quis, et alias sit? Quisquam beatae, vero inventore cupiditate ullam, quasi repellat esse hic maxime veritatis odio iusto suscipit!</p>
+                        <a href="#" class="btn">Download</a>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </section>
+
+    <script src="/js/index.js"></script>
     
 </body>
 </html>
