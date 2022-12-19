fa760f5fae064f4e0dc5331bc8d6d31c578a8275
ikkrit <johan.car@protonmail.com>
2022-12-19 23:16:33 +0100

    about

--- views/home/download.html.php
+++ views/home/download.html.php
@@ -1,6 +1,26 @@
-<section class="about container">
+<section class="">
 
-    <h2>About</h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae quaerat ipsam repellendus eaque pariatur aliquam deleniti praesentium ea, adipisci voluptates voluptas aut, minima est eveniet alias libero quod commodi assumenda.</p>
+    <div class="about container">
+        <h2>About</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae quaerat ipsam repellendus eaque pariatur aliquam deleniti praesentium ea, adipisci voluptates voluptas aut, minima est eveniet alias libero quod commodi assumenda.</p>
+    </div>
+
+    <div class="screenshots container">
+        <h2>Screenshots</h2>
+        <div class="screenshots-content">
+            <img src="/assets/img/home/slide/screenshot-img-1.jpeg" alt="">
+            <img src="/assets/img/home/slide/screenshot-img-2.jpeg" alt="">
+            <img src="/assets/img/home/slide/screenshot-img-3.jpeg" alt="">
+        </div>
+    </div>
+
+    <div class="download">
+        <h2>Download links</h2>
+        <div class="download-links">
+            <a href="#">For Android</a>
+            <a href="#">For IOS</a>
+            <a href="#">For Windows</a>
+        </div>
+    </div>
 
 </section>
